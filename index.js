dd4283b0a43f3fe7466af5e2ade7612dd17f21a7
NathaliRodrigues <142436453+NathaliRodrigues@users.noreply.github.com>
2023-09-04 17:20:10 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,79 @@
+// Nathali Rodrigues
+/*const filme ={
+nome:`velozes e furiosos 10`,
+diretor: `Louis Leterrier`,
+anodelancamento: 2023,
+elenco: [`Vim Diesel`,`Jason Momoa`, `Jordana Brewster`],
+assisti: true
+}
+console.log(filme.nome);
+console.log(filme.diretor);
+console.log(filme.anodelancamento);
+console.log(filme[`elenco`]);
+console.log(filme[`assisti`]);
+
+const pessoa ={
+    nome:"Diego",
+    idade:19,
+    generomusical:"pagode"
+};
+console.log("O nome da pessoa é " + pessoa.nome, "ele tem " + pessoa.idade, "anos e gosta muito de " + pessoa.generomusical);
+
+const filme ={
+    nome:`velozes e furiosos 10`,
+    diretor: `Louis Leterrier`,
+    anodelancamento: 2023,
+    elenco: [`Vin Diesel`,`Jason Momoa`, `Jordana Brewster`],
+    assisti: true
+    }
+    console.log(filme.nome);
+    console.log(filme.diretor);
+    console.log(filme.anodelancamento);
+    console.log(filme[`elenco`]);
+    console.log(filme[`assisti`]);
+
+filme.personagem = ["torreto", "Dante", "Mia"];
+    console.log(filme.elenco[0]);
+    console.log(filme.elenco [0] + "->" + filme.personagem[0]);
+    console.log(filme.elenco [1] + "->" + filme.personagem[1]);
+    console.log(filme.elenco [2] + "->" + filme.personagem[2]);
+    filme.personagem[0] = "xuxa"
+    console.log(filme.elenco[0]);
+
+const usuario = {
+    nome:`prof`,
+    idade:`25`,
+    email:`prof@senacrs.com.br`,
+    cidade:`são paulo`
+}
+
+
+const novousuario = {...usuario,
+nome:`João`,
+idade: 28
+}
+console.log(usuario);
+console.log(novousuario);
+
+const listaDeNomes = ["Mika", "Paula", "vitor"]
+const copiaListaDeNomes = [...listaDeNomes]
+console.log(copiaListaDeNomes)
+
+
+
+    const pessoa ={
+        nome:`Diego`,
+        idade:`19`
+    };
+
+function testepessoa(teste){
+const novapessoa = {...teste,
+    comidas:["lasanha", "pizza"],
+    melhorAmigo: {nome:"Alex",
+    idade:`20` }
+    };
+    console.log("O nome da pessoa é " + novapessoa.nome, "e suas comidas preferidas são "
+     + novapessoa.comidas, "Seu melhor amigo se chama " + novapessoa.melhorAmigo.nome, "e tem ", + novapessoa.idade, "anos");
+};
+testepessoa(pessoa)
+*/
